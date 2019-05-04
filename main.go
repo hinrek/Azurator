a6349ca516a01d251a41ae12a76e4b412a785d00
Hinrek Saar <hinrek@gmail.com>
2019-05-04 13:56:52 +0300

    Added support for Travis CI and refactored code
    Added .travis.yml for testing Travis CI with golang.
    Commented out stuff in main.go that is not needed currently.

--- main.go
+++ main.go
@@ -6,7 +6,6 @@ import (
 	"time"
 
 	"github.com/hinrek/Azure-migrator/models/configuration"
-	"github.com/hinrek/Azure-migrator/models/git"
 	"github.com/hinrek/Azure-migrator/models/project"
 )
 
@@ -14,7 +13,7 @@ var (
 	conf          configuration.Conf
 	projects      project.Projects
 	singleProject project.Project
-	repositories  git.Repositories
+	//repositories  git.Repositories
 )
 
 func init() {
@@ -38,17 +37,17 @@ func main() {
 	log.Println("GET Project List")
 	projectList := projects.List(sourceOrganization, sourceAPIVersion, sourcePersonalAccessToken, client)
 
-	log.Println("GET Single Project")
-	projectID := "726e343e-b03d-4633-85de-6642d4b850fa"
-	getProject := singleProject.Get(sourceOrganization, projectID, sourceAPIVersion, sourcePersonalAccessToken, client)
+	//log.Println("GET Single Project")
+	//projectID := "726e343e-b03d-4633-85de-6642d4b850fa"
+	//getProject := singleProject.Get(sourceOrganization, projectID, sourceAPIVersion, sourcePersonalAccessToken, client)
 
-	log.Printf("Project: %+v\n", getProject)
+	//log.Printf("Project: %+v\n", getProject)
 
 	//log.Println("POST Single project")
 	//postProject := getProject.Create(destinationOrganization, destinationAPIVersion, destinationPersonalAccessToken, client)
 	//log.Println(postProject.Body)
 
-	log.Println("POST All Project")
+	log.Println("POST All Projects")
 	for _, item := range projectList.Project {
 		log.Println("Project: ", item.Name)
 
@@ -56,8 +55,8 @@ func main() {
 		getProject.Create(destinationOrganization, destinationAPIVersion, destinationPersonalAccessToken, client)
 	}
 
-	log.Println("GET Repositories List")
-	repositoriesList := repositories.List(sourceOrganization, "Project%20source", sourceAPIVersion, sourcePersonalAccessToken, client)
+	//log.Println("GET Repositories List")
+	//repositoriesList := repositories.List(sourceOrganization, "Project%20source", sourceAPIVersion, sourcePersonalAccessToken, client)
 
-	log.Printf("Repositories: %+v\n", repositoriesList)
+	//log.Printf("Repositories: %+v\n", repositoriesList)
 }
